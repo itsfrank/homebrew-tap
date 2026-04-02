53e4f5dc790d77e09b9f670345187ec526cd4f30
itsfrank <7297152+itsfrank@users.noreply.github.com>
2026-04-02 03:53:02 +0000

    Update portpal Homebrew metadata for v0.2.1

--- Formula/portpal.rb
+++ Formula/portpal.rb
@@ -1,8 +1,8 @@
 class Portpal < Formula
   desc "CLI and background service for managing forwarded SSH ports"
   homepage "https://github.com/itsfrank/portpal"
-  url "https://github.com/itsfrank/portpal/releases/download/v0.2.0/portpal-cli.tar.gz"
-  sha256 "abf8b8604c2533aec38e32f9cc7d9154289dda6595ec2e4e575050a970f6a32e"
+  url "https://github.com/itsfrank/portpal/releases/download/v0.2.1/portpal-cli.tar.gz"
+  sha256 "9526afedcb6c5395fa0b015c09494642b706f19a68757c210498fa52a182c04c"
   license "MIT"
 
   depends_on arch: :arm64
